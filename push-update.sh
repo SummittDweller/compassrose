3d18581034e2c4fa31a3a0a70377232317ddd8e1
SummittDweller <summitt.dweller@gmail.com>
2019-11-19 22:48:43 -0600

    Tiny tweak

--- push-update.sh
+++ push-update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd ~/Projects/compassrose
-perl -i.bak -lpe 'BEGIN { sub inc { my ($num) = @_; ++$num } } s/(build = )(\d+)/$1 . (inc($2))/eg' config.toml
+perl -i.bak -lpe  'BEGIN { sub inc { my ($num) = @_; ++$num } } s/(build = )(\d+)/$1 . (inc($2))/eg' config.toml
 docker image build -t compassrose .
 docker login
 docker tag compassrose summittdweller/compassrose:latest
